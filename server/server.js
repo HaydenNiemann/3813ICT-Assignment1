f8cf0724fdf4ef5a3b9f60ac5ab75ffa315f7bd9
Hayden Niemann <hayden.niemann93@gmail.com>
2024-09-05 13:39:51 +1000

    Added JSON storage for channel data

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const socketHandler = require('./sockets'); 
+const fs = require('fs'); 
 
 const app = express();
 app.use(cors());
@@ -9,12 +10,23 @@ app.use(cors());
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "http://localhost:4200", 
+    origin: "http://localhost:4200",
     methods: ["GET", "POST"]
   }
 });
 
-socketHandler.connect(io);
+let channelMessages = {};
+
+function loadData() {
+  if (fs.existsSync('data.json')) {
+    const data = fs.readFileSync('data.json');
+    channelMessages = JSON.parse(data);
+  }
+}
+
+loadData();
+
+socketHandler.connect(io, channelMessages);
 
 const PORT = 3000;
 server.listen(PORT, () => {
--- server/sockets.js
+++ server/sockets.js
@@ -1,56 +1,55 @@
-const channelMessages = {}; 
+const fs = require('fs'); 
 
-module.exports.connect = function (io) {
+module.exports.connect = function (io, channelMessages) {
+  
+  function saveData() {
+    fs.writeFileSync('data.json', JSON.stringify(channelMessages, null, 2)); 
+  }
+
   io.on('connection', (socket) => {
     console.log('A user connected');
 
-    
     socket.on('joinChannel', (channelName) => {
       socket.join(channelName);
       console.log(`User joined channel: ${channelName}`);
 
-      
       if (channelMessages[channelName]) {
         const sortedMessages = channelMessages[channelName]
           .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp))
           .slice(0, 5);
         socket.emit('chatHistory', sortedMessages);
       } else {
-        socket.emit('chatHistory', []); 
+        socket.emit('chatHistory', []);
       }
     });
 
-    
     socket.on('sendMessage', ({ channelName, message, user }) => {
       const newMessage = {
-        user: user || 'Anonymous', 
+        user: user || 'Anonymous',
         message,
         timestamp: new Date(),
       };
 
-      
       if (!channelMessages[channelName]) {
         channelMessages[channelName] = [];
       }
       channelMessages[channelName].push(newMessage);
 
-      
       io.to(channelName).emit('newMessage', newMessage);
+
+      saveData();
     });
 
-   
     socket.on('deleteChannel', (channelName) => {
-      delete channelMessages[channelName]; 
+      delete channelMessages[channelName];
       console.log(`Channel ${channelName} and its messages have been deleted.`);
+      
+      
+      saveData();
     });
 
-    
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
   });
 };
-
-
-
-
